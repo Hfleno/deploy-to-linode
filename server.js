b47740cc284de68ff84683c87d0a57188da32e74
Hfleno <hfleno@gmail.com>
2024-11-04 21:01:58 +0100

    new server

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-// Importing the http module
-const http = require("http")
+import express from 'express'
 
-// Creating server
-const server = http.createServer((req, res) => {
-    // Sending the response
-    res.write("This is deployed by github actions and restarted by PM2 via sh-script Hurra")
-    res.end();
+const app = express()
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.post('/mail', (req, res) => {
+  const mail = req.body
+  console.log(mail)
+  // Save the data of user that was sent by the client
+
+  // Send a response to client that will show that the request was successfull.
+  res.status(200).json({ msg: 'success' })
 })
 
-// Server listening to port 3000.
-server.listen((3000), () => {
-    console.log("Server is Running");
+app.get('/', (req, res) => {
+  res.send('new server')
+})
+
+const port = 3000 // or any other port number you prefer
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
 })
